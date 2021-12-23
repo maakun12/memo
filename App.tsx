fbac5917985913d65b83c4c492b8335f4f24b268
maakun12 <amram.system@gmail.com>
2021-12-23 22:56:14 +0900

    add src dir

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import App from './src/App'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
-export default function App () {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+const AppContainer = () => {
+  return <App />
 }
+
+export default AppContainer
