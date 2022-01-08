c87e384a03f63b48cb4e5753884b00ecd9810743
maakun12 <amram.system@gmail.com>
2022-01-08 20:35:56 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+const Stack = createNativeStackNavigator()
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    </View>
+  )
+}
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+        name="memo"
+        component={HomeScreen}
+      />
+      </Stack.Navigator>
+      <StatusBar style='auto' />
+    </NavigationContainer>
   )
 }
